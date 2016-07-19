12a186fbe8e5d1c2dabb6b1258feb08340c8a5e2
Andrea Parodi <andrea@parro.it>
2016-07-19 17:57:17 +0200

    Process improved

--- index.js
+++ index.js
@@ -1,60 +1,39 @@
 'use strict';
 
-let reduxStore;
-
-/*
- *	weird trick to retrieve redux store
- *	there should be a better way
- */
-exports.middleware = store => next => action => {
-	if (!reduxStore) {
-		reduxStore = store;
-	}
-	next(action);
-};
-
-/*
- *	remove default title setting by set Terms onTitle prop to noop
- *	maybe it's better to implement a map state to event?
- */
-exports.decorateTerms = (Terms, {React}) => {
-	return class extends React.Component {
-		render() {
-			const props = Object.assign({}, this.props, {onTitle: () => {}});
-			return React.createElement(Terms, props);
-		}
+const mkOnTerminal = (originalOnTerminal, onTitle) => term => {
+	term.setWindowTitle = title => {
+		onTitle(title);
 	};
+
+	if (originalOnTerminal) {
+		originalOnTerminal(term);
+	}
 };
 
 /*
- *	decorate Term component to extend hterm by handling onTerminal event
- *  extends hterm object to set `setWindowTitle` method and dispatch a redux action in it.
+ *  extends hterm object to set `setWindowTitle` method and run onTitle event.
  */
 exports.decorateTerm = (Term, {React}) => {
-	const onTerminal = originalOnTerminal => term => {
-		term.setWindowTitle = title => {
-			if (reduxStore) {
-				// temporary use active tab, there should be a way to
-				// find the uid of this particular hterm instance...
-				const uid = reduxStore.getState().sessions.activeUid;
-				reduxStore.dispatch({
-					type: 'SESSION_SET_XTERM_TITLE',
-					uid,
-					title
-				});
-			}
-		};
-
-		if (originalOnTerminal) {
-			originalOnTerminal(term);
-		}
-	};
-
 	return class extends React.Component {
 		render() {
 			const originalOnTerminal = this.props.onTerminal;
-			const props = Object.assign({}, this.props, {onTerminal: onTerminal(originalOnTerminal)});
+			const onTerminal = mkOnTerminal(originalOnTerminal, this.props.onTitle);
+			const props = Object.assign({}, this.props, {onTerminal});
+
+			/*
+			 *	remove default title setting by set Term onTitle prop to noop
+			 */
+			props.onTitle = () => {};
 			return React.createElement(Term, props);
 		}
 	};
+};
+
+/*
+ *	remove process title setting by canceling action
+ */
+exports.middleware = store => next => action => {
+	if (action.type !== 'SESSION_SET_PROCESS_TITLE') {
+		next(action);
+	}
 };
